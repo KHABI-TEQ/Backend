f21f57ac6b9c94fffd794b3ce853709ae0961a2d
Abasay <abdulsalamasheem@gmail.com>
2025-04-03 16:58:19 +0100

    fix

---
+++ declarations.d.ts
@@ -0,0 +1,16 @@
+declare module 'jsonwebtoken';
+declare module 'node-cron';
+declare module 'nodemailer';
+declare module 'passport';
+declare module 'bcryptjs';
+declare module 'morgan';
+declare module 'cookie-parser';
+declare module 'cors';
+declare module 'passport-jwt';
+
+// Fix for the req.user issue
+declare namespace Express {
+  export interface Request {
+    user?: any;
+  }
+}
